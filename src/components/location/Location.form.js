a9b07d288a4a8eabe0065d519401b91e92f41ee7
Luis Eduardo <lebolivarc@gmail.com>
2018-06-26 10:20:03 -0500

    Ajuste por cambio de versión

--- src/components/location/Location.form.js
+++ src/components/location/Location.form.js
@@ -1,28 +1,14 @@
-'use strict';
+import baseEditForm from '../base/Base.form';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+import LocationEditMap from './editForm/Location.edit.map';
 
-exports.default = function () {
-  for (var _len = arguments.length, extend = Array(_len), _key = 0; _key < _len; _key++) {
-    extend[_key] = arguments[_key];
-  }
-
-  return _Base2.default.apply(undefined, extend.concat([[{
-    label: 'Map',
-    key: 'map',
-    weight: 1,
-    components: _LocationEdit2.default
-  }]]));
-};
-
-var _Base = require('../base/Base.form');
-
-var _Base2 = _interopRequireDefault(_Base);
-
-var _LocationEdit = require('./editForm/Location.edit.map');
-
-var _LocationEdit2 = _interopRequireDefault(_LocationEdit);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+export default function(...extend) {
+  return baseEditForm(...extend, [
+    {
+      label: 'Map',
+      key: 'map',
+      weight: 1,
+      components: LocationEditMap
+    }
+  ]);
+}
