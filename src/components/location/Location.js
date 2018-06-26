a044096890a86525d7bd5836491bdb60473f3d6a
Luis Eduardo <lebolivarc@gmail.com>
2018-06-26 13:35:39 -0500

    Cambios en location.js

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,120 +1,84 @@
-'use strict';
+/* global google */
+import BaseComponent from '../base/Base';
+import Formio from '../../Formio';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _Base = require('../base/Base');
-
-var _Base2 = _interopRequireDefault(_Base);
-
-var _Formio = require('../../Formio');
-
-var _Formio2 = _interopRequireDefault(_Formio);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* global google */
-
-
-var LocationComponent = function (_BaseComponent) {
-  _inherits(LocationComponent, _BaseComponent);
-
-  _createClass(LocationComponent, null, [{
-    key: 'schema',
-    value: function schema() {
-      for (var _len = arguments.length, extend = Array(_len), _key = 0; _key < _len; _key++) {
-        extend[_key] = arguments[_key];
+export default class LocationComponent extends BaseComponent {
+  static schema(...extend) {
+    return BaseComponent.schema({
+      type: 'location',
+      label: 'Location',
+      key: 'location',
+      map: {
+        key: '',
+        region: '',
+        gmapId: '',
+        autocompleteOptions: {}
       }
-
-      return _Base2.default.schema.apply(_Base2.default, [{
-        type: 'location',
-        label: 'Location',
-        key: 'location',
-        map: {
-          key: '',
-          region: '',
-          gmapId: '',
-          autocompleteOptions: {}
-        }
-      }].concat(extend));
-    }
-  }, {
-    key: 'builderInfo',
-    get: function get() {
-      return {
-        title: 'Location',
-        group: 'advanced',
-        icon: 'fa fa-map',
-        weight: 500,
-        schema: LocationComponent.schema()
-      };
-    }
-  }]);
-
-  function LocationComponent(component, options, data) {
-    _classCallCheck(this, LocationComponent);
-
-    // Get the source for Google Maps API
-    var _this = _possibleConstructorReturn(this, (LocationComponent.__proto__ || Object.getPrototypeOf(LocationComponent)).call(this, component, options, data));
-
-    var src = 'https://maps.googleapis.com/maps/api/js?v=3&libraries=places&callback=googleMapsCallback';
-    if (component.map && component.map.key) {
-      src += '&key=' + component.map.key;
-    }
-    if (component.map && component.map.region) {
-      src += '&region=' + component.map.region;
-    }
-
-    _Formio2.default.requireLibrary('googleMaps', 'google.maps.places', src);
-    return _this;
+    }, ...extend);
   }
 
-  _createClass(LocationComponent, [{
-    key: 'build',
+  static get builderInfo() {
+    return {
+      title: 'Location',
+      group: 'advanced',
+      icon: 'fa fa-map',
+      weight: 500,
+      schema: LocationComponent.schema()
+    };
+  }
 
-    value: function build() {
-      this.element = this.ce('div', {
-        id: this.id,
-        class: 'map-container'
-      });
+  constructor(component, options, data) {
+    super(component, options, data);
 
-      this.element.component = this;
-      this.initGoogleMap();
-
-      this.input = this.createInput(this.element);
-      this.addInput(this.input, this.element);
-
-  /*      var gmapElement = this.ce('div', {
-        id: this.component.map.gmapId,
-        style: 'min-height: 300px; height: calc(100vh - 600px);'
-      });
-      this.element.appendChild(gmapElement); */
-    } 
-  }, {
-    key: 'setValue',
-    value: function setValue(value, flags) {
-      flags = this.getFlags.apply(this, arguments);
-      flags.noValidate = true;
-      _get(LocationComponent.prototype.__proto__ || Object.getPrototypeOf(LocationComponent.prototype), 'setValue', this).call(this, value, flags);
+    // Get the source for Google Maps API
+    let src = 'https://maps.googleapis.com/maps/api/js?v=3&libraries=places&callback=googleMapsCallback';
+    if (component.map && component.map.key) {
+      src += `&key=${component.map.key}`;
     }
-  }, {
-    key: 'addInput',
-    value: function addInput(input, container) {
-      var _this2 = this;
+    if (component.map && component.map.region) {
+      src += `&region=${component.map.region}`;
+    }
+    Formio.requireLibrary('googleMaps', 'google.maps.places', src);
+  }
 
-      _get(LocationComponent.prototype.__proto__ || Object.getPrototypeOf(LocationComponent.prototype), 'addInput', this).call(this, input, container);
-      var that = this;
-      _Formio2.default.libraryReady('googleMaps').then(function () {
-        var autocompleteOptions = {};
+  get defaultSchema() {
+    return LocationComponent.schema();
+  }
+
+  get emptyValue() {
+    return '';
+  }
+
+  build() {
+    this.element = this.ce('div', {
+      id: this.id,
+      class: 'map-container'
+    });
+    this.element.component = this;
+    this.initGoogleMap();
+    this.input = this.createInput(this.element);
+    this.addInput(this.input, this.element);
+
+    /* const gmapElement = this.ce('div', {
+      id: this.component.map.gmapId,
+      style: 'min-height: 300px; height: calc(100vh - 600px);'
+    });
+    this.element.appendChild(gmapElement); */
+  }
+
+  setValue(value, flags) {
+    flags = this.getFlags.apply(this, arguments);
+    flags.noValidate = true;
+    super.setValue(value, flags);
+  }
+
+  addInput(input, container) {
+    super.addInput(input, container);
+    const that = this;
+    Formio.libraryReady('googleMaps').then(() => {
+      let autocompleteOptions = {};
+      if (this.component.map) {
+        autocompleteOptions = this.component.map.autocompleteOptions || {};
 
         if (_this2.component.map) {
           var types = [];
@@ -124,119 +88,109 @@ var LocationComponent = function (_BaseComponent) {
             types.push("" + _this2.component.map.gmapTypes);
             autocompleteOptions.types = types;
           }
-          else if (_this2.component.map.gmapTypes == 'address'){
+          else if (_this2.component.map.gmapTypes == 'address') {
             types = [];
             autocompleteOptions.types = types;
           }
         }
-        
-        var autocomplete = new google.maps.places.Autocomplete(input, autocompleteOptions);
-        autocomplete.addListener('place_changed', function () {
-          that.marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            console.log("Autocomplete's returned place contains no geometry");
-            return;
-          }
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            that.map.fitBounds(place.geometry.viewport);
-          } else {
-            that.map.setCenter(place.geometry.location);
-            that.map.setZoom(17); // Why 17? Because it looks good.
-          }
-          that.marker.setIcon( /** @type {google.maps.Icon} */{
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-          });
-          that.marker.setPosition(place.geometry.location);
-          that.marker.setVisible(true);
-          that.setValue(place.name);
-        });
-      });
-    }
-  }, {
-    key: 'elementInfo',
-    value: function elementInfo() {
-      var info = _get(LocationComponent.prototype.__proto__ || Object.getPrototypeOf(LocationComponent.prototype), 'elementInfo', this).call(this);
-      info.attr.class += ' Gmap-search';
-      return info;
-    }
-  }, {
-    key: 'initGoogleMap',
-    value: function initGoogleMap() {
-      var _this3 = this;
-
-      _Formio2.default.libraryReady('googleMaps').then(function () {
-        var defaultLatlng = new google.maps.LatLng(45.5041482, -73.5574125);
-        var options = {
-          zoom: 19,
-          center: defaultLatlng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          styles: [{
-            'featureType': 'poi',
-            'stylers': [{
-              'visibility': 'off'
-            }]
-          }, {
-            'featureType': 'transit',
-            'stylers': [{
-              'visibility': 'off'
-            }]
-          }]
-        };
-
-        var mapElement = document.getElementById(_this3.component.map.gmapId);
-        if (!mapElement) {
+      }
+      const autocomplete = new google.maps.places.Autocomplete(input, autocompleteOptions);
+      autocomplete.addListener('place_changed', () => {
+        that.marker.setVisible(false);
+        const place = autocomplete.getPlace();
+        if (!place.geometry) {
+          console.log("Autocomplete's returned place contains no geometry");
           return;
         }
-        _this3.map = new google.maps.Map(mapElement, options);
-        _this3.addMarker(defaultLatlng, 'Default Marker', _this3.map);
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          that.map.fitBounds(place.geometry.viewport);
+        }
+        else {
+          that.map.setCenter(place.geometry.location);
+          that.map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        that.marker.setIcon(/** @type {google.maps.Icon} */({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+        }));
+        that.marker.setPosition(place.geometry.location);
+        that.marker.setVisible(true);
+        that.setValue(place.name);
       });
-    }
-  }, {
-    key: 'addMarker',
-    value: function addMarker(latlng, title, map) {
-      var that = this;
-      this.marker = new google.maps.Marker({
-        position: latlng,
-        map: map,
-        title: title,
-        draggable: true
-      });
-      this.marker.addListener('dragend', function (event) {
-        var geocoder = new google.maps.Geocoder();
-        var latlng = { lat: parseFloat(event.latLng.lat()), lng: parseFloat(event.latLng.lng()) };
-        geocoder.geocode({ 'location': latlng }, function (results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            if (results[1]) {
-              that.setValue(results[0].formatted_address);
-            } else {
-              console.log('No results found');
-            }
-          } else {
-            console.log('Geocoder failed due to: ' + status);
+    });
+  }
+
+  elementInfo() {
+    const info = super.elementInfo();
+    info.attr.class += ' Gmap-search';
+    return info;
+  }
+
+  initGoogleMap() {
+    Formio.libraryReady('googleMaps').then(() => {
+      const defaultLatlng = new google.maps.LatLng(45.5041482, -73.5574125);
+      const options = {
+        zoom: 19,
+        center: defaultLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+          {
+            'featureType': 'poi',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'transit',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
           }
-        });
+        ]
+      };
+
+      const mapElement = document.getElementById(this.component.map.gmapId);
+      if (!mapElement) {
+        return;
+      }
+      this.map = new google.maps.Map(mapElement, options);
+      this.addMarker(defaultLatlng, 'Default Marker', this.map);
+    });
+  }
+
+  addMarker(latlng, title, map) {
+    const that = this;
+    this.marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: title,
+      draggable: true
+    });
+    this.marker.addListener('dragend', (event) => {
+      const geocoder = new google.maps.Geocoder;
+      const latlng = { lat: parseFloat(event.latLng.lat()), lng: parseFloat(event.latLng.lng()) };
+      geocoder.geocode({ 'location': latlng }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+            that.setValue(results[0].formatted_address);
+          }
+          else {
+            console.log('No results found');
+          }
+        }
+        else {
+          console.log(`Geocoder failed due to: ${status}`);
+        }
       });
-    }
-  }, {
-    key: 'defaultSchema',
-    get: function get() {
-      return LocationComponent.schema();
-    }
-  }, {
-    key: 'emptyValue',
-    get: function get() {
-      return '';
-    }
-  }]);
-
-  return LocationComponent;
-}(_Base2.default);
-
-exports.default = LocationComponent;
+    });
+  }
+}
